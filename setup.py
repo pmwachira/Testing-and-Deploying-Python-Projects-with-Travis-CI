92a71acc45f055bd1866fe29223d015b54e0ad37
pmwachira <pmwachira@gmail.com>
2018-09-11 10:14:23 +0300

    Added Travis CI

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='travistest',
       packages=['travistest'],
       version='1.0',
       description='Testing the Travis CI',
-      url='https://github.com/pmwachira/Testing-and-Deploying-Python-Projects-with-Travis-CI.git'
+      url='https://github.com/pmwachira/Testing-and-Deploying-Python-Projects-with-Travis-CI'
       )
