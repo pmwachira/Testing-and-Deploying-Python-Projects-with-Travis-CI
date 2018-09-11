7112f361b92bfa83491eb62aebe8128a29e61be0
mushirih <pmwachira@gmail.com>
2018-09-11 07:16:43 +0300

    Commit Message

---
+++ travistest_/Operations.py
@@ -0,0 +1,16 @@
+class Operations:
+	def __init__(self):
+		pass
+
+	def add(self, x,y):
+		return x+y
+
+	def subtract(self, x,y):
+		return x-y
+
+	def decrement(self, x):
+		return x-1
+
+	def increment(self, x):
+		return x+1
+
