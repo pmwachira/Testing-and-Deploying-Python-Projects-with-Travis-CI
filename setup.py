90d7931ef410050a79c6d5a9e13e4207de6b6c04
mushirih <pmwachira@gmail.com>
2018-09-11 07:14:27 +0300

    Commit Message

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='travistest',
+      packages=['travistest'],
+      version='1.0',
+      description='Testing the Travis CI',
+      url='https://github.com/pmwachira/Testing-and-Deploying-Python-Projects-with-Travis-CI.git'
+      )
